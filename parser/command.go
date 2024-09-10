310d3e35afabbc303c8e96913cfa08b55b573074
Marcelo Mesquita <marcelofosmesquita@gmail.com>
2024-09-10 17:06:03 -0300

    fix: corrected wrong toolNames

--- parser/command.go
+++ parser/command.go
@@ -3,6 +3,8 @@ package parser
 import (
 	"strings"
 	"unicode"
+
+	"github.com/rs/zerolog/log"
 )
 
 // parseModule retorna o nome do módulo correto para a URL.
@@ -50,13 +52,55 @@ func parseTool(toolName string) string {
 	switch toolName {
 	case "EncloseMultiPatch":
 		return "enclose-multipatch"
-	}
-	var tool string = strings.Clone(toolName)
-	for i, r := range toolName[1:] {
-		if !unicode.IsUpper(r) {
-			continue
+	case "Import3DFiles":
+		return "import-3d-files"
+	case "Intersect3D":
+		return "intersect-3d-3d-analyst-"
+	case "ArcGISProject":
+		return ""
+	case "CreateSpaceTimeCubeMDRasterLayer":
+		return "createcubefrommdrasterlayer"
+	case "EmergingHotSpotAnalysis":
+		return "emerginghotspots"
+	case "VisualizeSpaceTimeCube3D":
+		return "visualizecube3d"
+	case "VisualizeSpaceTimeCube2D":
+		return "visualizecube2d"
+	default:
+		var tool string = strings.Clone(toolName)
+		indexes := make([]int, 0, len(tool))
+		for i, r := range toolName[1:] {
+			if !unicode.IsUpper(r) {
+				continue
+			}
+			if unicode.IsDigit(rune(toolName[i])) {
+				log.Debug().
+					Str("Tool", toolName).
+					Int("CmdIndex", i).
+					Str("Char", string(r)).
+					Msgf("%s is a digit", string(toolName[i]))
+				continue
+			}
+			log.Debug().
+				Str("Tool", toolName).
+				Int("CmdIndex", i).
+				Str("Char", string(r)).
+				Str("PreviousWords", toolName[:i+1]).
+				Str("NextWords", toolName[i+1:]).
+				Send()
+			indexes = append(indexes, i+1)
 		}
-		tool = tool[:i+1] + "-" + tool[i+1:]
+		if len(indexes) > 0 {
+			var parts []string
+			start := 0
+			for _, index := range indexes {
+				parts = append(parts, tool[start:index])
+				start = index
+			}
+			parts = append(parts, tool[indexes[len(indexes)-1]:])
+
+			tool = strings.Join(parts, "-")
+		}
+		return strings.ToLower(tool)
 	}
-	return strings.ToLower(tool)
 }
