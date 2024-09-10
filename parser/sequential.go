c008e6c7d8e6ebaa289abb2ae5f45d325e4dcab7
Marcelo Mesquita <marcelofosmesquita@gmail.com>
2024-09-10 17:06:25 -0300

    refactor: refactored sequentialParse

--- parser/sequential.go
+++ parser/sequential.go
@@ -1,6 +1,8 @@
 package parser
 
 import (
+	"strings"
+
 	"github.com/marcelo-fm/arctracker/model"
 	"github.com/marcelo-fm/arctracker/scraper"
 	"github.com/rs/zerolog/log"
@@ -18,27 +20,57 @@ func sequentialParse(searcher Searcher, s *scraper.Scraper) ([]model.License, er
 	log.Info().Msg("Done")
 	log.Info().Msg("Parsing arcpy commands...")
 	baseURL := viper.GetString("baseURL")
-	licenses := make([]model.License, 0, len(contentArr))
+	urls := make([]string, 0, len(contentArr))
 	for _, content := range contentArr {
 		cmd := parseCommand(content)
 		if cmd == "" {
+			log.Debug().Msgf("There is no command in match: %s", content.Text)
 			continue
 		}
+		log.Debug().Str("Cmd", cmd).Send()
 		url := createURL(cmd)
 		if url == "" {
 			continue
 		}
+		log.Debug().Str("URL", url).Send()
+		urls = append(urls, url)
+	}
+	uniqueURLs := removeDuplicates(urls)
+	log.Debug().Msgf("%v", strings.Join(uniqueURLs, "\n"))
+	licenses := make([]model.License, 0, len(uniqueURLs))
+	for _, url := range uniqueURLs {
 		s.SetupLicenseScraper()
-		log.Info().Msg("Done")
 		log.Info().Msg("Scraping license information...")
-		defer log.Info().Msg("Done")
-		license := s.Scrape(baseURL + url)
-		if license.Title != "" && license.Name != "" {
-			licenses = append(licenses, license)
+		scrapeURL := baseURL + url
+		license, err := s.Scrape(scrapeURL)
+		if err != nil {
+			log.Error().Err(err).Msgf("Error in scraping url %s", scrapeURL)
+			return nil, err
 		}
+		log.Info().Msg("Done")
+		if license.Title == "" && license.Name == "" {
+			log.Debug().Msg("License does not have Title and Name")
+			continue
+		}
+		log.Debug().Msgf("Appending %s to licenses", license.Name)
+		licenses = append(licenses, license)
 	}
 	if licenses == nil {
 		return nil, nil
 	}
 	return licenses, nil
+}
+
+func removeDuplicates(strings []string) []string {
+	uniqueStrings := make(map[string]bool)
+	result := []string{}
+
+	for _, str := range strings {
+		if !uniqueStrings[str] {
+			uniqueStrings[str] = true
+			result = append(result, str)
+		}
+	}
+
+	return result
 }
