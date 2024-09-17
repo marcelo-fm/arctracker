25111dd80e44032da2d4ea0e2484cf8d36fb8cb6
Marcelo Mesquita <marcelofosmesquita@gmail.com>
2024-09-17 11:51:49 -0300

    feat: finish pipelineParse function
    this function does the url parsing within a pipeline

--- internal/parser/pipeline.go
+++ internal/parser/pipeline.go
@@ -2,10 +2,57 @@ package parser
 
 import (
 	"context"
+	"strings"
 	"sync"
 
 	"github.com/marcelo-fm/arctracker/internal/model"
+	"github.com/marcelo-fm/arctracker/internal/scraper"
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/viper"
 )
+
+func pipelineParse(searcher Searcher, s *scraper.Scraper) ([]model.License, error) {
+	log.Info().Msg("Searching for arcpy commands...")
+	contentArr, err := searcher.Search()
+	if err != nil {
+		return nil, err
+	}
+	log.Info().Msg("Done")
+	log.Info().Msg("Parsing arcpy commands...")
+	baseURL := viper.GetString("baseURL")
+	urls := make([]string, 0, len(contentArr))
+	ctx := context.TODO()
+	matchc := genContent(ctx, contentArr)
+	cmdc := parseCommandChannel(ctx, matchc)
+	urlc := createURLChannel(ctx, cmdc)
+	for u := range urlc {
+		urls = append(urls, u)
+	}
+	uniqueURLs := removeDuplicates(urls)
+	log.Debug().Msgf("%v", strings.Join(uniqueURLs, "\n"))
+	licenses := make([]model.License, 0, len(uniqueURLs))
+	for _, url := range uniqueURLs {
+		s.SetupLicenseScraper()
+		log.Info().Msg("Scraping license information...")
+		scrapeURL := baseURL + url
+		license, err := s.Scrape(scrapeURL)
+		if err != nil {
+			log.Error().Err(err).Msgf("Error in scraping url %s", scrapeURL)
+			return nil, err
+		}
+		log.Info().Msg("Done")
+		if license.Title == "" && license.Name == "" {
+			log.Debug().Msg("License does not have Title and Name")
+			continue
+		}
+		log.Debug().Msgf("Appending %s to licenses", license.Name)
+		licenses = append(licenses, license)
+	}
+	if licenses == nil {
+		return nil, nil
+	}
+	return licenses, nil
+}
 
 // genContent gera o chanel de bytes, que representa cada achado da palavra chave com
 // regex.
@@ -30,6 +77,9 @@ func parseCommandChannel(ctx context.Context, in <-chan model.Match) <-chan stri
 		defer close(out)
 		for match := range in {
 			cmd := parseCommand(match)
+			if cmd == "" {
+				continue
+			}
 			select {
 			case out <- cmd:
 			case <-ctx.Done():
