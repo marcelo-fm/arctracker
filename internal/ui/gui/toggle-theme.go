2f68d3bab53f1efbb9b5816c4c7b431651ceb7df
Marcelo Mesquita <marcelofosmesquita@gmail.com>
2024-09-18 14:30:58 -0300

    feat: toggle-theme toggles the logo

--- internal/ui/gui/toggle-theme.go
+++ internal/ui/gui/toggle-theme.go
@@ -4,9 +4,11 @@ import (
 	"image/color"
 
 	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
+	"github.com/marcelo-fm/arctracker/assets"
 )
 
 type forcedVariant struct {
@@ -18,13 +20,35 @@ func (f *forcedVariant) Color(name fyne.ThemeColorName, _ fyne.ThemeVariant) col
 	return f.Theme.Color(name, f.variant)
 }
 
-func CreateToggleTheme(a fyne.App) fyne.CanvasObject {
+func CreateToggleTheme(a fyne.App, imgContainer *fyne.Container) fyne.CanvasObject {
+	var logo fyne.Resource
+	switch a.Settings().ThemeVariant() {
+	case theme.VariantDark:
+		logo = assets.LogoDark
+	case theme.VariantLight:
+		logo = assets.LogoLight
+	}
+	image := canvas.NewImageFromResource(logo)
+	image.ScaleMode = canvas.ImageScaleFastest
+	image.FillMode = canvas.ImageFillOriginal
+	imgContainer.RemoveAll()
+	imgContainer.Add(image)
 	themes := container.NewGridWithColumns(2,
 		widget.NewButton("Dark", func() {
 			a.Settings().SetTheme(&forcedVariant{Theme: theme.DefaultTheme(), variant: theme.VariantDark})
+			image := canvas.NewImageFromResource(assets.LogoDark)
+			image.ScaleMode = canvas.ImageScaleFastest
+			image.FillMode = canvas.ImageFillOriginal
+			imgContainer.RemoveAll()
+			imgContainer.Add(image)
 		}),
 		widget.NewButton("Light", func() {
 			a.Settings().SetTheme(&forcedVariant{Theme: theme.DefaultTheme(), variant: theme.VariantLight})
+			image := canvas.NewImageFromResource(assets.LogoLight)
+			image.ScaleMode = canvas.ImageScaleFastest
+			image.FillMode = canvas.ImageFillOriginal
+			imgContainer.RemoveAll()
+			imgContainer.Add(image)
 		}),
 	)
 	return themes
