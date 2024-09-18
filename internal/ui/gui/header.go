5975476eb302da8fa3a17af45d2241570ed75eee
Marcelo Mesquita <marcelofosmesquita@gmail.com>
2024-09-18 14:29:52 -0300

    feat: added image logo for light and dark mode

--- internal/ui/gui/header.go
+++ internal/ui/gui/header.go
@@ -2,26 +2,17 @@ package gui
 
 import (
 	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/layout"
-	"github.com/marcelo-fm/arctracker/assets"
-	"github.com/rs/zerolog/log"
 )
 
 func AddHeader(a fyne.App, content fyne.CanvasObject) fyne.CanvasObject {
-	var logo fyne.CanvasObject
-	logo = layout.NewSpacer()
-	f, err := assets.Assets.Open("arctracker-logo.png")
-	if err != nil {
-		log.Debug().Err(err).Msg("Error in reading logo file")
-		logo = canvas.NewImageFromReader(f, "arctracker-logo")
-	}
-	themes := CreateToggleTheme(a)
+	imgContainer := container.NewStack()
+	themes := CreateToggleTheme(a, imgContainer)
 	header := container.NewHBox(
-		logo,
+		imgContainer,
 		layout.NewSpacer(),
-		JustifyCenter(themes),
+		ItemsCenter(JustifyCenter(themes)),
 	)
 	return container.NewPadded(container.NewBorder(header, BottomInfo(), nil, nil, content))
 }
