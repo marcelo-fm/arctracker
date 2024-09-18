e581deb5a98a688fe2a6ddd925d1c0b2cdf3672b
Marcelo Mesquita <marcelofosmesquita@gmail.com>
2024-09-18 14:27:29 -0300

    feat: added resources

--- assets/embed.go
+++ assets/embed.go
@@ -3,11 +3,34 @@ package assets
 import (
 	"embed"
 	"io/fs"
+
+	"fyne.io/fyne/v2"
 )
 
 //go:embed *
 var Assets embed.FS
 
+//go:embed arctracker-logo-dark.png
+var LogoContentDark []byte
+
+//go:embed arctracker-logo-light.png
+var LogoContentLight []byte
+
+//go:embed icons/moon.png
+var IconContentDark []byte
+
+//go:embed icons/sun.png
+var IconContentLight []byte
+
 func LoadResource(name string) ([]byte, error) {
 	return fs.ReadFile(Assets, name)
+}
+
+var LogoDark, LogoLight, IconDark, IconLight fyne.Resource
+
+func init() {
+	LogoDark = fyne.NewStaticResource("logo-dark", LogoContentDark)
+	LogoLight = fyne.NewStaticResource("logo-light", LogoContentLight)
+	IconDark = fyne.NewStaticResource("icon-dark", IconContentDark)
+	IconLight = fyne.NewStaticResource("icon-light", IconContentLight)
 }
