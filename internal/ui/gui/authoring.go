5062337ac90913ab562b10c61b0e65ba252b32ed
Marcelo Mesquita <marcelofosmesquita@gmail.com>
2024-09-19 13:03:11 -0300

    fix: corrected linkedin

--- internal/ui/gui/authoring.go
+++ internal/ui/gui/authoring.go
@@ -14,7 +14,7 @@ func NewBottomInfo() *fyne.Container {
 	author := widget.NewLabel(lang.L("Author") + ": Marcelo Foschiera de Mesquita")
 	github := widget.NewHyperlink("Github", nil)
 	github.SetURLFromString("https://github.com/marcelo-fm")
-	linkedn := widget.NewHyperlink("Linkedn", nil)
-	linkedn.SetURLFromString("www.linkedin.com/in/marcelo-mesquita-535795233")
-	return container.NewHBox(author, github)
+	linkedin := widget.NewHyperlink("Linkedin", nil)
+	linkedin.SetURLFromString("www.linkedin.com/in/marcelo-mesquita-535795233")
+	return container.NewHBox(author, github, linkedin)
 }
